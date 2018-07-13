76bb05d1bce2e6f17139601258fd91c41de287f8
ryoichi-obara <ryoichi-obara@users.noreply.github.com>
2018-07-13 11:43:13 +0900

    Add JSDoc and comments.

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ const { TO_BUCKET } = process.env;
 /**
  * Implement your conditional code.
  * @param {string} key S3 Bucket key name
+ * @return true:copy.
  */
 const isCopyTarget = (key) => {
   if (key) {
@@ -30,12 +31,30 @@ exports.handler = async (event) => {
 
   const params = { Bucket: FROM_BUCKET };
   do {
+    // List bucket objects.
     const data = await S3.listObjectsV2(params).promise();
+
+    // Binary:
+    // await Promise.all(
+    //   data.Contents.filter(content => isCopyTarget(content)).map((content) => {
+    //     const s3Stream = S3.getObject({
+    //       Bucket: FROM_BUCKET,
+    //       Key: content.Key,
+    //     }).createReadStream();
+    //     return S3.upload({
+    //       Bucket: TO_BUCKET,
+    //       Key: content.Key,
+    //       Body: s3Stream,
+    //     }).promise();
+    //   }),
+    // );
+
+    // Text:
     await Promise.all(
-      data.Contents.filter(content => isCopyTarget(content.Key)).map(e => S3.copyObject({
+      data.Contents.filter(content => isCopyTarget(content.Key)).map(content => S3.copyObject({
         Bucket: TO_BUCKET,
-        CopySource: `${FROM_BUCKET}/${e.Key}`,
-        Key: e.Key,
+        CopySource: `${FROM_BUCKET}/${content.Key}`,
+        Key: content.Key,
       }).promise()),
     );
 
@@ -47,6 +66,7 @@ exports.handler = async (event) => {
     // NextContinuationToken: '1xaDw9pQqvhAuXiUwnhiRM6bgm2MCyQ/+T4H7PhPB6KLD69OHPWMdPxma5rguqZ/J5+QUjScYtJYVLswa1SaoIRXCdi/d9exH1++R9lAYQGOmORabyv5KqD7y66ftZzJe0x7sQ4vgup+FEBnhnIIL4='
 
     if (data.NextContinuationToken) {
+      // Continue to retrieve next 1000 objects.
       params.ContinuationToken = data.NextContinuationToken;
     } else {
       break;
